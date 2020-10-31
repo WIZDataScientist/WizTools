416f1db31f1e5b73f7ebf12083ea1cb7a561d384
Wilson Zhou <wilsonzhou92@gmail.com>
2020-10-31 18:28:11 +0100

    a problem with bat files fized

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 python setup.py sdist
-python pip install dist/wiz_ds_toolz-0.1.tar.gz
+pip install dist/wiz_ds_toolz-0.1.tar.gz
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 
 python setup.py sdist
-python pip uninstall dist/wiz_ds_toolz
-python pip install dist/wiz_ds_toolz-0.1.tar.gz
+pip uninstall dist/wiz_ds_toolz
+pip install dist/wiz_ds_toolz-0.1.tar.gz
